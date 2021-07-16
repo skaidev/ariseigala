031df1a505da9a81fc2fb6840ecfca50a03681b8
stormzy22 <aawweb23@gmail.com>
2021-07-16 16:17:36 +0100

    video comp ready

--- frontend/src/pages/tv/index.tsx
+++ frontend/src/pages/tv/index.tsx
@@ -1,61 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
+import VideoPlayer from "components/videoComp/VideoPlayer";
 import TvLayout from "Layout/TvLayout";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Tv = (): JSX.Element => {
-  const [currentVidTime, setCurrentVidTime] = useState(null);
-  const [vidDurationTime, setVidDurationTime] = useState(null);
-  const [range, setRange] = useState({
-    max: "",
-    value: "0",
-  });
-  const [play_pause, setPlay_Pause] = useState(false);
+  const [src, setSrc] = useState("");
 
-  const vidRef = useRef();
-
-  const updateProgress = (e): void => {
-    const { duration, currentTime } = e.target;
-    const currentHrs = Math.floor(currentTime / 3600);
-    const durationHrs = Math.floor(duration / 3600);
-    const currentMin = Math.floor(currentTime / 60);
-    const currentSec = Math.floor(currentTime - currentMin * 60);
-    const durationMin = Math.floor(duration / 60);
-    const durationSec = Math.floor(duration - durationMin * 60);
-    setCurrentVidTime(
-      `${currentHrs > 0 ? currentHrs + ":" : ""}${currentMin}:${currentSec}`
-    );
-    setVidDurationTime(
-      `${durationHrs > 0 ? durationHrs + ":" : ""}${durationMin}:${durationSec}`
-    );
-  };
-  const setProgress = (e) => {
-    const { duration, currentTime } = e.target;
-    setRange({
-      ...range,
-      max: String(duration),
-      value: String(currentTime),
-    });
-  };
-  const customProgress = (e) => {
-    const video = document.getElementById("video");
-    const { currentTime, duration } = video;
-
-    const { value } = e.target;
-    console.log((currentTime / duration) * 100 + ":" + value);
-  };
-  const controlPlayPause = () => {
-    const video = document.getElementById("video");
-    const vidState = video.paused || video.ended;
-    if (vidState) {
-      video.play();
-      setPlay_Pause(true);
-    }
-    if (!vidState) {
-      video.pause();
-      setPlay_Pause(false);
-    }
-  };
   return (
     <TvLayout>
       <Tvwrapper>
@@ -80,67 +31,7 @@ const Tv = (): JSX.Element => {
             <div className="tv-body-main-video d-flex flex-column flex-lg-row">
               <div className="left">
                 <div className="video-container bg-dark py-3">
-                  <figure>
-                    <video
-                      controls
-                      onTimeUpdate={(e) => {
-                        updateProgress(e);
-                        setProgress(e);
-                      }}
-                      className="w-100"
-                      ref={vidRef}
-                      id="video"
-                    >
-                      <source src="video/tears-of-steel-battle-clip-medium.mp4" />
-                    </video>
-                    <div className="container  video-controls">
-                      <div
-                        className="mb-1 video-controls-progress bg-light rounded-0 position-relative"
-                        onClick={setProgress}
-                      >
-                        {/* <progress
-                          id="file"
-                          value={range.value}
-                          max={range.max}
-                          className="d-block w-100 m-0 progress rounded-0 position-absolute"
-                          onClick={(e) => console.log(e.target.value)}
-                        ></progress> */}
-                        <input
-                          type="range"
-                          className="form-range m-0 mb-2"
-                          value={range.value}
-                          min="0"
-                          max={range.max}
-                          id="customRange3"
-                          onChange={customProgress}
-                        />
-                      </div>
-                      <div className="d-flex align-items-center">
-                        <div className="d-flex">
-                          <button className="btn text-white">
-                            <i className="fas fa-step-backward"></i>
-                          </button>
-                          <button
-                            className="btn text-white"
-                            onClick={controlPlayPause}
-                          >
-                            {!play_pause ? (
-                              <i className="fas fa-play"></i>
-                            ) : (
-                              <i className="fas fa-pause"></i>
-                            )}
-                          </button>
-                          <button className="btn text-white">
-                            <i className="fas fa-step-forward"></i>
-                          </button>
-                        </div>
-                        <div className="text-white">
-                          {currentVidTime}/{vidDurationTime}
-                        </div>
-                        <div></div>
-                      </div>
-                    </div>
-                  </figure>
+                  {src && <VideoPlayer />}
                 </div>
                 <div className="video-title">
                   <div className="border-bottom py-2 border-dark">
@@ -214,15 +105,22 @@ const Tv = (): JSX.Element => {
               </div>
               <div className="right">
                 <div className="row right-main row-cols-1 g-3">
-                  {Cards.map((card) => (
-                    <div className="col border-0 right-main-col" key={card}>
+                  {Video.map((card) => (
+                    <div className="col border-0 right-main-col" key={card.id}>
                       <div className="card right-main-col-card bg-transparent border-0 d-flex flex-row">
-                        <div className="img">
-                          <img
-                            src="images/Frame 1.png"
-                            alt=""
+                        <div className="img d-flex justify-content-end align-items-end">
+                          <video
+                            src={card.src}
                             className="card-img"
+                            id={card.ids}
+                            onClick={(e) => setSrc(e.target.id)}
                           />
+                          <mark
+                            className="bg-dark text-white mb-2 me-2"
+                            style={{ zIndex: 1 }}
+                          >
+                            11:00
+                          </mark>
                         </div>
                         <div className="ms-1 rigth-card-body card-body p-0">
                           <p className="p-0 mb-2 fw-bolder fs-6">
@@ -255,3 +153,22 @@ const Tvwrapper = styled.div`
 const Cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 // https://www.youtube.com/watch?v=ysz5S6PUM-U
 // "http://iandevlin.github.io/mdn/video-player/video/tears-of-steel-battle-clip-medium.mp4"
+// "video/tears-of-steel-battle-clip-medium.mp4";
+
+const Video = [
+  {
+    id: 1,
+    src: "http://localhost:3000/video/smile.mp4",
+    ids: "smile",
+  },
+  {
+    id: 2,
+    src: "http://localhost:3000/video/laugh.mp4",
+    ids: "laugh",
+  },
+  {
+    id: 3,
+    src: "http://iandevlin.github.io/mdn/video-player/video/tears-of-steel-battle-clip-medium.mp4",
+    ids: "tears-of-steel-battle-clip-medium",
+  },
+];
