0705888c818c30bc2cda4585d96ac6c25099918f
stormzy22 <aawweb23@gmail.com>
2021-07-23 22:17:05 +0100

    pdf working

--- frontend/src/pages/magazine/[id].tsx
+++ frontend/src/pages/magazine/[id].tsx
@@ -5,20 +5,27 @@ const Pdf = (): JSX.Element => {
    return (
       <PdfLayout>
          <div>
-            <div className="top-bar">
-               <button className="btn" id="prev-page">
-                  <i className="fas fa-arrow-circle-left"></i> Prev Page
-               </button>
-               <button className="btn" id="prev-page">
-                  Next Page <i className="fas fa-arrow-circle-right"></i>
-               </button>
-
-               <span className="page-info">
-                  Page <span id="page-num"></span> of
-                  <span id="page-count"></span>
-               </span>
+            <canvas id="pdf-render" className="mx-auto d-block mb-3"></canvas>
+            <div className="container">
+               <div className="progress mb-2">
+                  <div className="progress-bar"></div>
+               </div>
+               <div className="mb-2 fw-bold d-flex justify-content-end">
+                  <span className="percent-read"></span>% read
+               </div>
+               <div className="top-bar d-flex align-items-center justify-content-center">
+                  <button className="btn" id="prev-page">
+                     <i className="fas fa-arrow-circle-left"></i> Prev Page
+                  </button>
+                  <span className="page-info">
+                     Page <span id="page-num"></span> of
+                     <span id="page-count"></span>
+                  </span>
+                  <button className="btn" id="next-page">
+                     Next Page <i className="fas fa-arrow-circle-right"></i>
+                  </button>
+               </div>
             </div>
-            <canvas id="pdf-render"></canvas>
          </div>
       </PdfLayout>
    );
