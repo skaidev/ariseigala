1bcf2331560e2a761d9035a26c5816e9baf4c03b
stormzy22 <aawweb23@gmail.com>
2021-07-23 15:53:25 +0100

    working on the pdf

--- frontend/src/Layout/TvLayout.tsx
+++ frontend/src/Layout/TvLayout.tsx
@@ -3,23 +3,23 @@ import TvHeader from "components/tvComp/TvHeader";
 import React from "react";
 import styled from "styled-components";
 
-const PdfLayout = ({
-  children,
+const TvLayout = ({
+   children,
 }: {
-  children: React.ReactChild;
+   children: React.ReactChild;
 }): JSX.Element => {
-  return (
-    <TvMain id="tv-layout">
-      <TvHeader />
-      <main>{children}</main>
-      <MagazineFooter />
-    </TvMain>
-  );
+   return (
+      <TvMain id="tv-layout">
+         <TvHeader />
+         <main>{children}</main>
+         <MagazineFooter />
+      </TvMain>
+   );
 };
 
-export default PdfLayout;
+export default TvLayout;
 const TvMain = styled.div`
-  min-height: 100vh;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
+   min-height: 100vh;
+   display: grid;
+   grid-template-rows: auto 1fr auto;
 `;
