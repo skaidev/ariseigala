2e54a9926d73b3058051ab4450f7548f963e8053
Peter Akaliro <pin4eva@hotmail.com>
2021-07-29 15:39:42 +0100

    removed console.log

--- frontend/src/pages/articles/[category]/[slug].tsx
+++ frontend/src/pages/articles/[category]/[slug].tsx
@@ -3,16 +3,35 @@ import React from "react";
 import Image from "next/image";
 import Advertisement from "components/Advertisement";
 import styled from "styled-components";
+import Link from "next/link";
 const single = (): JSX.Element => {
   return (
     <ArticleLayout>
       <SingleArticle>
         <div className="container single-article-wrapper">
           <div className="single-article-wrapper-advertisement py-3 mb-3">
-            <p>
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <Link href="/">
+                    <a className="link-dark text-decoration-none">Home</a>
+                  </Link>
+                </li>
+                <li className="breadcrumb-item">
+                  <Link href="/articles/news">
+                    <a className="link-dark text-decoration-none">Category</a>
+                  </Link>
+                </li>
+                <li className="breadcrumb-item active" aria-current="page">
+                  something here...
+                </li>
+              </ol>
+            </nav>
+            {/* <p>
               <span className="text-muted">Home / Culture and tourism /</span>
               Vehicula cong...
-            </p>
+            </p> */}
+
             <Advertisement />
           </div>
           <div className="single-article-1">
@@ -27,103 +46,39 @@ const single = (): JSX.Element => {
           </div>
           <div className="single-article-2">
             <div className="single-article-2-img py-2">
-              <Image
-                src="/images/Rectangle 30.png"
-                alt="Landscape picture"
-                width={`${1000}%`}
-                height={500}
-                layout="responsive"
-              />
+              <Image src="/images/Rectangle 30.png" alt="Landscape picture" width={`${1000}%`} height={500} layout="responsive" />
             </div>
             <div className="single-article-2-main mt-3">
               <div className="px-3 text-justify mb-2">
                 <p className="p-0 ">
-                  Urna morbi eleifend porttitor leo posuere. Amet, facilisis
-                  pellentesque commodo quis proin orci morbi adipiscing
-                  pharetra. Sollicitudin arcu auctor praesent phasellus egestas.
-                  Tempor sodales tellus urna, etiam a, sed sed. Tortor nec
-                  consectetur libero malesuada. Ut tellus vivamus dignissim
-                  volutpat sed semper non at netus. Integer curabitur nunc,
-                  mauris lacus. Turpis nulla id sapien est leo, risus. Aenean
-                  sagittis, sed et imperdiet vitae vel, lacus. Vitae at sagittis
-                  vestibulum eget porttitor iaculis enim. Eu egestas amet id
-                  aliquet et, sit ullamcorper in venenatis. Volutpat dui aliquam
-                  morbi leo sed. Leo vivamus lobortis nunc id proin aliquam
-                  felis. Mattis convallis lacus id tellus ac lacus. Duis diam
-                  ac, commodo ut netus. Justo, amet, nisl, egestas nulla. Tellus
-                  quam a arcu nec turpis metus nisl viverra. Egestas massa sed
-                  dolor sed. Sed dignissim molestie justo, gravida massa.
-                  Volutpat proin neque sed maecenas. Nunc nec et gravida purus
-                  penatibus urna Sit sit faucibus felis consectetur consectetur
-                  donec diam ac id. Erat et pulvinar malesuada dictum nunc. Et
-                  mauris, sed laoreet mauris, sapien. Morbi at pellentesque
+                  Urna morbi eleifend porttitor leo posuere. Amet, facilisis pellentesque commodo quis proin orci morbi adipiscing pharetra. Sollicitudin arcu auctor praesent phasellus egestas. Tempor sodales tellus urna, etiam a, sed sed.
+                  Tortor nec consectetur libero malesuada. Ut tellus vivamus dignissim volutpat sed semper non at netus. Integer curabitur nunc, mauris lacus. Turpis nulla id sapien est leo, risus. Aenean sagittis, sed et imperdiet vitae
+                  vel, lacus. Vitae at sagittis vestibulum eget porttitor iaculis enim. Eu egestas amet id aliquet et, sit ullamcorper in venenatis. Volutpat dui aliquam morbi leo sed. Leo vivamus lobortis nunc id proin aliquam felis.
+                  Mattis convallis lacus id tellus ac lacus. Duis diam ac, commodo ut netus. Justo, amet, nisl, egestas nulla. Tellus quam a arcu nec turpis metus nisl viverra. Egestas massa sed dolor sed. Sed dignissim molestie justo,
+                  gravida massa. Volutpat proin neque sed maecenas. Nunc nec et gravida purus penatibus urna Sit sit faucibus felis consectetur consectetur donec diam ac id. Erat et pulvinar malesuada dictum nunc. Et mauris, sed laoreet
+                  mauris, sapien. Morbi at pellentesque
                 </p>
                 <p>
-                  egestas natoque vestibulum mauris id. Egestas feugiat viverra
-                  dignissim amet urna, quis. Egestas eget morbi at sapien
-                  viverra. Elementum pharetra ut proin et accumsan, adipiscing
-                  amet, posuere leo. Sem scelerisque feugiat nunc donec
-                  scelerisque et sit. Scelerisque viverra arcu ultricies morbi.
-                  Ultricies quam neque sit felis morbi commodo duis. Egestas ac
-                  cras amet risus duis. Etiam vitae adipiscing dui orci.
-                  Elementum posuere egestas placerat felis placerat nullam
-                  congue tincidunt. Ipsum elit orci adipiscing tristique turpis
-                  ac nullam sit. Praesent senectus turpis varius sit sit tellus,
-                  netus. Scelerisque adipiscing sed est nunc. A feugiat quis
-                  fermentum, sit arcu. Magna nunc, facilisi nisl velit sit sit.
-                  Lacus malesuada condimentum in lectus et nibh odio risus. Arcu
-                  felis eu consectetur velit faucibus dictumst viverra vitae
-                  blandit. Quis posuere purus amet maecenas enim morbi nisl et.
-                  Scelerisque luctus neque cras feugiat fames vitae tincidunt
-                  ornare. Tempus sed aliquam nulla mattis tellus. Nunc
-                  ullamcorper adipiscing cursus varius proin nunc. Urna lobortis
-                  ac aliquam viverra nisi viverra amet. Placerat dolor at
-                  aliquam nisl. Donec et at platea magna libero. Pellentesque
-                  proin molestie dignissim augue justo varius sed. Dictum
-                  molestie feugiat dignissim amet turpis at sit. Aliquam
-                  vulputate adipiscing elementum faucibus scelerisque nullam sit
-                  ut. Orci, neque malesuada vitae vitae interdum id ac vel.
-                  Adipiscing nunc sit venenatis sociis augue quisque. Metus,
-                  nunc, commodo nisi massa, gravida viverra eu. Tincidunt at
-                  sollicitudin aliquam sociis quam ultricies. Convallis iaculis
-                  massa ullamcorper enim vitae cursus. In nisi sodales lectus
-                  tristique dui in magna rutrum et. Vulputate mattis aliquet ut
-                  sit tellus mauris in nunc. Felis habitasse a ut quam ac.
-                  Pharetra ut morbi mus diam platea arcu massa. Cras tortor,
-                  aliquam malesuada quis ultricies accumsan. Tincidunt cursus
-                  tincidunt cursus pulvinar venenatis viverra. Sed leo sed nunc,
-                  ullamcorper eu. Nunc gravida in vitae eu. Adipiscing in
-                  aliquam lacus et ligula. Rhoncus ac mauris justo, aliquam eget
+                  egestas natoque vestibulum mauris id. Egestas feugiat viverra dignissim amet urna, quis. Egestas eget morbi at sapien viverra. Elementum pharetra ut proin et accumsan, adipiscing amet, posuere leo. Sem scelerisque feugiat
+                  nunc donec scelerisque et sit. Scelerisque viverra arcu ultricies morbi. Ultricies quam neque sit felis morbi commodo duis. Egestas ac cras amet risus duis. Etiam vitae adipiscing dui orci. Elementum posuere egestas
+                  placerat felis placerat nullam congue tincidunt. Ipsum elit orci adipiscing tristique turpis ac nullam sit. Praesent senectus turpis varius sit sit tellus, netus. Scelerisque adipiscing sed est nunc. A feugiat quis
+                  fermentum, sit arcu. Magna nunc, facilisi nisl velit sit sit. Lacus malesuada condimentum in lectus et nibh odio risus. Arcu felis eu consectetur velit faucibus dictumst viverra vitae blandit. Quis posuere purus amet
+                  maecenas enim morbi nisl et. Scelerisque luctus neque cras feugiat fames vitae tincidunt ornare. Tempus sed aliquam nulla mattis tellus. Nunc ullamcorper adipiscing cursus varius proin nunc. Urna lobortis ac aliquam
+                  viverra nisi viverra amet. Placerat dolor at aliquam nisl. Donec et at platea magna libero. Pellentesque proin molestie dignissim augue justo varius sed. Dictum molestie feugiat dignissim amet turpis at sit. Aliquam
+                  vulputate adipiscing elementum faucibus scelerisque nullam sit ut. Orci, neque malesuada vitae vitae interdum id ac vel. Adipiscing nunc sit venenatis sociis augue quisque. Metus, nunc, commodo nisi massa, gravida viverra
+                  eu. Tincidunt at sollicitudin aliquam sociis quam ultricies. Convallis iaculis massa ullamcorper enim vitae cursus. In nisi sodales lectus tristique dui in magna rutrum et. Vulputate mattis aliquet ut sit tellus mauris in
+                  nunc. Felis habitasse a ut quam ac. Pharetra ut morbi mus diam platea arcu massa. Cras tortor, aliquam malesuada quis ultricies accumsan. Tincidunt cursus tincidunt cursus pulvinar venenatis viverra. Sed leo sed nunc,
+                  ullamcorper eu. Nunc gravida in vitae eu. Adipiscing in aliquam lacus et ligula. Rhoncus ac mauris justo, aliquam eget
                 </p>
                 <p>
-                  sapien netus purus nullam. Diam dictum neque faucibus
-                  fermentum pellentesque pretium, arcu ut. Sit mauris, malesuada
-                  aliquam commodo tortor eget velit. Posuere fames non nunc
-                  massa venenatis, a et habitasse nunc. Aliquet eget varius
-                  sollicitudin placerat. Purus mollis cursus adipiscing
-                  porttitor. Duis suspendisse vel at justo, diam, amet mauris at
-                  bibendum. Malesuada et, consequat arcu proin nulla nascetur
-                  morbi et convallis. Amet gravida id est dolor commodo, diam
-                  ultrices tempus. Bibendum amet eu mauris morbi urna feugiat
-                  fermentum. Et, id malesuada ridiculus odio maecenas tempus. At
-                  nunc, ultricies ultricies viverra vitae. Et leo lectus diam
-                  purus id tortor lectus eget urna. Mollis vel, morbi posuere
-                  malesuada accumsan. Id mi, lectus adipiscing amet. Nec turpis
-                  et integer cras. Egestas vitae dis cras urna, sit. Egestas
-                  lobortis semper ornare sit. Dui diam nulla netus ipsum neque
-                  purus. Congue rutrum sit volutpat mi nulla. Sed morbi
-                  tristique tempus sit at enim. Diam id elit porttitor maecenas
-                  vitae facilisi aliquam gravida. Morbi vel feugiat purus a
-                  aliquet.
+                  sapien netus purus nullam. Diam dictum neque faucibus fermentum pellentesque pretium, arcu ut. Sit mauris, malesuada aliquam commodo tortor eget velit. Posuere fames non nunc massa venenatis, a et habitasse nunc. Aliquet
+                  eget varius sollicitudin placerat. Purus mollis cursus adipiscing porttitor. Duis suspendisse vel at justo, diam, amet mauris at bibendum. Malesuada et, consequat arcu proin nulla nascetur morbi et convallis. Amet gravida
+                  id est dolor commodo, diam ultrices tempus. Bibendum amet eu mauris morbi urna feugiat fermentum. Et, id malesuada ridiculus odio maecenas tempus. At nunc, ultricies ultricies viverra vitae. Et leo lectus diam purus id
+                  tortor lectus eget urna. Mollis vel, morbi posuere malesuada accumsan. Id mi, lectus adipiscing amet. Nec turpis et integer cras. Egestas vitae dis cras urna, sit. Egestas lobortis semper ornare sit. Dui diam nulla netus
+                  ipsum neque purus. Congue rutrum sit volutpat mi nulla. Sed morbi tristique tempus sit at enim. Diam id elit porttitor maecenas vitae facilisi aliquam gravida. Morbi vel feugiat purus a aliquet.
                 </p>
                 <div className="div py-2">
-                  <Image
-                    src="/images/Rectangle 31.png"
-                    alt="Landscape picture"
-                    width={`${1000}%`}
-                    height={500}
-                    layout="responsive"
-                  />
+                  <Image src="/images/Rectangle 31.png" alt="Landscape picture" width={`${1000}%`} height={500} layout="responsive" />
                   {/* <img src="" alt="" loading="lazy" /> */}
                 </div>
               </div>
