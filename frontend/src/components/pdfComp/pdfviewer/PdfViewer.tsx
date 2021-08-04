1e9b60d5e1ab3bc06eb2bf5d1a0987d17f7e47dc
stormzy22 <aawweb23@gmail.com>
2021-08-04 08:38:23 +0100

    flipping

--- frontend/src/components/pdfComp/pdfviewer/PdfViewer.tsx
+++ frontend/src/components/pdfComp/pdfviewer/PdfViewer.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import styled from "styled-components";
+import HTMLFlipBook from "react-pageflip";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PdfViewer({ file }: { file: string }): JSX.Element {
@@ -52,16 +54,24 @@ export default function PdfViewer({ file }: { file: string }): JSX.Element {
               <ul className="list-group">
                 {numContents().map((content, i) => (
                   <li
-                    className={`list-group-item d-flex align-items-center border-0 c-pointer ${pageNumber == content ? "bg-secondary" : ""}`}
+                    className={`list-group-item d-flex align-items-center border-0 c-pointer ${
+                      pageNumber == content ? "bg-secondary" : ""
+                    }`}
                     key={i}
                     onClick={() => {
                       setPageNumber(content);
                       setContentList(false);
-                    }}>
+                    }}
+                  >
                     <span className="fw-bold">-</span>
                     <p className="m-0 p-0 px-2 flex-fill">
-                      <b className="d-block text-center fw-bolder fs-5 mb-2">A in nunc purus urna nulla eu gravida quisque. </b>
-                      <span className="float-end pe-5">Nisl, ac eget ac nibh. Vel dui auctor ultricies quisque a nibh.</span>
+                      <b className="d-block text-center fw-bolder fs-5 mb-2">
+                        A in nunc purus urna nulla eu gravida quisque.{" "}
+                      </b>
+                      <span className="float-end pe-5">
+                        Nisl, ac eget ac nibh. Vel dui auctor ultricies quisque
+                        a nibh.
+                      </span>
                     </p>
                     <span className="fw-bold">{content}</span>
                   </li>
@@ -76,31 +86,71 @@ export default function PdfViewer({ file }: { file: string }): JSX.Element {
           <i className="fas fa-2x fa-bars"></i>
         </button>
         <div className="d-flex justify-content-between gap-3">
-          <button className="btn" disabled={zoom <= 1} onClick={() => setZoom((e) => e - 1)}>
+          <button
+            className="btn"
+            disabled={zoom <= 1}
+            onClick={() => setZoom((e) => e - 1)}
+          >
             <i className="fas fa-search-minus fa-2x"></i>
           </button>
-          <button className="btn" disabled={zoom > 5} onClick={() => setZoom((e) => e + 1)}>
+          <button
+            className="btn"
+            disabled={zoom > 5}
+            onClick={() => setZoom((e) => e + 1)}
+          >
             <i className="fas fa-search-plus fa-2x"></i>
           </button>
         </div>
       </div>
-      <div className="container py-3 overflow mb-4 mx-auto shadow-sm d-flex justify-content-center">
-        <div className="">
-          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} renderMode="canvas">
-            <Page pageNumber={pageNumber} scale={zoom} className="w-100" />
+      <div className="container pdf-section py-3">
+        <div className="pdf-wrapper">
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            renderMode="canvas"
+            className="pdf-main bg-danger p-3"
+          >
+            <HTMLFlipBook
+              width={500}
+              height={1000}
+              className="pdf-flipper"
+              size="stretch"
+              autoSize={true}
+              mobileScrollSupport={true}
+              usePortrait={true}
+            >
+              {numContents().map((e, i) => (
+                <div className="demoPage" key={i}>
+                  <Page pageNumber={e} scale={zoom} className="w-100" />
+                </div>
+              ))}
+            </HTMLFlipBook>
           </Document>
         </div>
       </div>
       <div className="container mb-4">
         <div className="progress mb-3">
-          <div className="progress-bar-animated bg-info" style={{ width: `${(pageNumber / numPages) * 100}%`, transition: ".5s" }}></div>
+          <div
+            className="progress-bar-animated bg-info"
+            style={{
+              width: `${(pageNumber / numPages) * 100}%`,
+              transition: ".5s",
+            }}
+          ></div>
         </div>
         {/* ----------- */}
-        <p className="text-end fw-bold mb-3">{Math.floor((pageNumber / numPages) * 100)}% read</p>
+        <p className="text-end fw-bold mb-3">
+          {Math.floor((pageNumber / numPages) * 100)}% read
+        </p>
         {/* -------  */}
         <div className="d-flex justify-content-center">
           <div className="d-flex gap-3 align-items-center">
-            <button type="button" disabled={pageNumber <= 1} onClick={previousPage} className="btn ">
+            <button
+              type="button"
+              disabled={pageNumber <= 1}
+              onClick={previousPage}
+              className="btn "
+            >
               <i className="fas fa-2x fa-caret-left"></i>
             </button>
             <p className="m-0">
@@ -108,7 +158,12 @@ export default function PdfViewer({ file }: { file: string }): JSX.Element {
               {numPages || "--"}
             </p>
 
-            <button type="button" disabled={pageNumber >= numPages} onClick={nextPage} className="btn">
+            <button
+              type="button"
+              disabled={pageNumber >= numPages}
+              onClick={nextPage}
+              className="btn"
+            >
               <i className="fas fa-2x fa-caret-right"></i>
             </button>
           </div>
@@ -137,15 +192,19 @@ const PDFWrapper = styled.div`
       z-index: 1000000;
     }
   }
-  .overflow {
-    overflow: auto !important;
-    @media screen and (max-width: 580px) {
-      width: 90% !important;
-      max-width: 580px !important;
-    }
-    box-sizing: border-box;
-    .react-pdf__Page__annotations {
-      display: none;
+  .pdf-section {
+    .pdf-wrapper {
+      .pdf-main {
+        min-height: fit-content;
+        box-sizing: border-box;
+        overflow: auto;
+        .pdf-flipper {
+          .demoPage {
+            width: 50% !important;
+            margin: 1px;
+          }
+        }
+      }
     }
   }
 `;
